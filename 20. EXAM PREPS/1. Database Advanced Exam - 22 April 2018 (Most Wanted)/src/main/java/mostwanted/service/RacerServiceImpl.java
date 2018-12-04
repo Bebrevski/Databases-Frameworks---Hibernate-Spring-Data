eec95ff6a936aff7be7c1be088d58372a8634ff0
Bebrevski <stefan.bebrevski89@gmail.com>
2018-12-04 23:00:41 +0200

    Exam prep

---
+++ 20. EXAM PREPS/1. Database Advanced Exam - 22 April 2018 (Most Wanted)/src/main/java/mostwanted/service/RacerServiceImpl.java
@@ -0,0 +1,43 @@
+package mostwanted.service;
+
+import mostwanted.repository.RacerRepository;
+import mostwanted.util.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class RacerServiceImpl implements RacerService{
+
+    private static final String RACERS_JSON_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/files/racers.json";
+
+    private final RacerRepository racerRepository;
+    private final FileUtil fileUtil;
+
+    @Autowired
+    public RacerServiceImpl(RacerRepository racerRepository, FileUtil fileUtil) {
+        this.racerRepository = racerRepository;
+        this.fileUtil = fileUtil;
+    }
+
+    @Override
+    public Boolean racersAreImported() {
+        return this.racerRepository.count() > 0;
+    }
+
+    @Override
+    public String readRacersJsonFile() throws IOException {
+        return this.fileUtil.readFile(RACERS_JSON_FILE_PATH);
+    }
+
+    @Override
+    public String importRacers(String racersFileContent) {
+        return null;
+    }
+
+    @Override
+    public String exportRacingCars() {
+        return null;
+    }
+}
---
+++ 20. EXAM PREPS/1. Database Advanced Exam - 22 April 2018 (Most Wanted)/src/main/java/mostwanted/service/TownServiceImpl.java
@@ -0,0 +1,43 @@
+package mostwanted.service;
+
+import mostwanted.repository.TownRepository;
+import mostwanted.util.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class TownServiceImpl implements TownService {
+
+    private static final String TOWNS_JSON_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/files/towns.json";
+
+    private final TownRepository townRepository;
+    private final FileUtil fileUtil;
+
+    @Autowired
+    public TownServiceImpl(TownRepository townRepository, FileUtil fileUtil) {
+        this.townRepository = townRepository;
+        this.fileUtil = fileUtil;
+    }
+
+    @Override
+    public Boolean townsAreImported() {
+        return this.townRepository.count() > 0;
+    }
+
+    @Override
+    public String readTownsJsonFile() throws IOException {
+        return this.fileUtil.readFile(TOWNS_JSON_FILE_PATH);
+    }
+
+    @Override
+    public String importTowns(String townsFileContent) {
+        return null;
+    }
+
+    @Override
+    public String exportRacingTowns() {
+        return null;
+    }
+}
